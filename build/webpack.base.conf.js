264a7b3dc371b654475167425f21976b96232a69
everjupe13 <everjupe13@gmail.com>
2022-11-17 10:59:29 +0300

    switch to yarn, update sass all modules

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -109,7 +109,6 @@ module.exports = {
                         loader: 'sass-loader',
                         options: { 
                             sourceMap: true,
-                            outputStyle: 'compressed',
                         }
                     }
                 ]
